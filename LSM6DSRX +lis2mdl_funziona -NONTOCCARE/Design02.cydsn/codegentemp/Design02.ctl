cfff2e0fca41ae3f69c609b241ae4448dd733f3f
mgnocco <marina.gnocco@iit.it>
2024-08-02 16:51:31 +0200

    ok

--- LSM6DSRX +lis2mdl_funziona -NONTOCCARE/Design02.cydsn/codegentemp/Design02.ctl
+++ LSM6DSRX +lis2mdl_funziona -NONTOCCARE/Design02.cydsn/codegentemp/Design02.ctl
@@ -1,6 +1,6 @@
 -- ======================================================================
 -- Design02.ctl generated from Design02
--- 06/27/2024 at 16:21
+-- 08/02/2024 at 16:32
 -- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 -- ======================================================================
 
--- LSM6DSRX +lis2mdl_funziona -NONTOCCARE/Design02.cydsn/main.c
+++ LSM6DSRX +lis2mdl_funziona -NONTOCCARE/Design02.cydsn/main.c
@@ -15,6 +15,7 @@
 #include <project.h>
 int IMU_ack;
 
+#define N_DEVICES 1
 //uint8 SensorHub[6] = {LSM6DSRX_SENSOR_HUB_1, LSM6DSRX_SENSOR_HUB_2, LSM6DSRX_SENSOR_HUB_3, LSM6DSRX_SENSOR_HUB_4, LSM6DSRX_SENSOR_HUB_5, LSM6DSRX_SENSOR_HUB_6};
 struct st_imu_data g_imu, g_imuNew;
 uint8 Mag[2][10][6];
@@ -31,10 +32,6 @@ uint8 j;
 uint8 count;
 uint8 k;
 uint8 var = 1;
-
-
-
-
 
 int main()
 {   MY_TIMER_Start();                 
@@ -53,78 +50,71 @@ int main()
     uint8 who;
     UART_Start();
     Control_Reg_1_Write(0);
-    for (k=0;k<1; k++){
-    Control_Reg_1_Write(0);
-    WriteControlRegisterSPI(LSM6DSRX_CTRL1_XL,0xA0); //104 Hz (normal mode)
-    WriteControlRegisterSPI(LSM6DSRX_CTRL3_C,0x40); //104 Hz (normal mode)
-    CyDelay(1);
-    WriteControlRegisterSPI(LSM6DSRX_CTRL2_G,0xA0); //104 Hz (normal mode)   
+    for (k = 0; k < N_DEVICES; k++){
+        Control_Reg_1_Write(2);
+        WriteControlRegisterSPI(LSM6DSRX_CTRL1_XL,0xA0); //104 Hz (normal mode)
+        WriteControlRegisterSPI(LSM6DSRX_CTRL3_C,0x40); //104 Hz (normal mode)
+        WriteControlRegisterSPI(LSM6DSRX_CTRL2_G,0xA0); //104 Hz (normal mode)   
+        // WriteControlRegisterSPI(LSM6DSRX_FUNC_CFG_ACCESS, 0x00);
+        CyDelay(100);
+        OneShot_ReadRoutine(EXT_SENS_ADDR,LIS2MDL_WHO_AM_I); //LIS2MDL -->valore WHO_AM_I = 64
+        OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_A,0x00); //
+        CyDelay(1);
+        OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_A,0x00); //
+        OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_B,0x02);
+        OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_C,0x10);
+        Continuous_ReadRoutine(EXT_SENS_ADDR,LIS2MDL_OUTX_L_REG,0x06);
+    }
     
-   // WriteControlRegisterSPI(LSM6DSRX_FUNC_CFG_ACCESS, 0x00);
-    CyDelay(100);
-    OneShot_ReadRoutine(EXT_SENS_ADDR,LIS2MDL_WHO_AM_I); //LIS2MDL -->valore WHO_AM_I = 64
-    OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_A,0x00); //
-    CyDelay(1);
-    OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_A,0x00); //
-    OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_B,0x02);
-    OneShot_WriteRoutine(EXT_SENS_ADDR,LIS2MDL_CFG_REG_C,0x10);
-    Continuous_ReadRoutine(EXT_SENS_ADDR,LIS2MDL_OUTX_L_REG,0x06);
-  }
-    
-    Control_Reg_1_Write(0);
+    Control_Reg_1_Write(3);
     j = 0;
     count =0;
     
-     while(var){
+    /*  
+    while(var){
     var = bottone_Read();
     UART_PutChar(var);
     CyDelay(1);}
+    */
     
     for(;;){
-   
-    
-       t0 = (uint16)MY_TIMER_ReadCounter();
-        for (k=0;k<1; k++){
-        Control_Reg_1_Write(0);
-//      ------------------------------------------------------------------------READ MAG
-        do 
-        {
-            XLDA = ReadControlRegisterSPI(LSM6DSRX_STATUS_REG);          
-        }
-        while ((XLDA & 0b00000001) == 0);   
-    
-        do 
-        {
-            SENS_HUB_ENDOP = ReadControlRegisterSPI(LSM6DSRX_STATUS_MASTER_MAINPAGE);   
-        }
-        while ((SENS_HUB_ENDOP & 0b00000001) == 0);
+        t0 = (uint16)MY_TIMER_ReadCounter();
+        for (k = 0; k < N_DEVICES ; k++){
+            Control_Reg_1_Write(2);
+    //      ------------------------------------------------------------------------READ MAG
+            do 
+            {
+                XLDA = ReadControlRegisterSPI(LSM6DSRX_STATUS_REG);          
+            }
+            while ((XLDA & 0b00000001) == 0);   
+        
+            do 
+            {
+                SENS_HUB_ENDOP = ReadControlRegisterSPI(LSM6DSRX_STATUS_MASTER_MAINPAGE);   
+            }
+            while ((SENS_HUB_ENDOP & 0b00000001) == 0);
 
-        WriteControlRegisterSPI(LSM6DSRX_FUNC_CFG_ACCESS, 0x40);   
-        CyDelayUs(100);  
-        ReadControlRegisterSPI(LSM6DSRX_WHO_AM_I); 
+            WriteControlRegisterSPI(LSM6DSRX_FUNC_CFG_ACCESS, 0x40);   
+            CyDelayUs(100);  
+            ReadControlRegisterSPI(LSM6DSRX_WHO_AM_I); 
+               
+            for (i = 0; i < 6; i++){
+                Mag[k][j][i]= ReadControlRegisterSPI(LSM6DSRX_SENSOR_HUB_1 + i);  
+            }
            
-        for (i = 0; i < 6; i++){
-            Mag[k][j][i]= ReadControlRegisterSPI(LSM6DSRX_SENSOR_HUB_1 + i);  
-        }
-       
-        WriteControlRegisterSPI(LSM6DSRX_FUNC_CFG_ACCESS, 0x00);   
-        CyDelayUs(100);
-         
-      
-          
-        for (i = 0; i < 6; i++){
-            Accel[k][j][i] = ReadControlRegisterSPI(LSM6DSRX_OUTX_L_A + i);
-        }
+            WriteControlRegisterSPI(LSM6DSRX_FUNC_CFG_ACCESS, 0x00);   
+            CyDelayUs(100);
+                       
+            for (i = 0; i < 6; i++){
+                Accel[k][j][i] = ReadControlRegisterSPI(LSM6DSRX_OUTX_L_A + i);
+            }
 
-        for (i = 0; i < 6; i++){
-            Gyro[k][j][i] = ReadControlRegisterSPI(LSM6DSRX_OUTX_L_G + i);
+            for (i = 0; i < 6; i++){
+                Gyro[k][j][i] = ReadControlRegisterSPI(LSM6DSRX_OUTX_L_G + i);
+            }
         }
-}
-        Control_Reg_1_Write(0);
+        Control_Reg_1_Write(3);
 
-//}        
-
-     
 //      ---------------------------------------------------------------------READ AXELS
 
         //CyDelay(1);
@@ -136,7 +126,7 @@ int main()
         if (j > 9) {
             
             for(count = 0; count < 10; count++){ 
-                for (k=0;k<1;k++){
+                for (k = 0; k < N_DEVICES; k++){
                 UART_PutChar(count);
                 UART_PutChar(0);
             
@@ -161,9 +151,9 @@ int main()
                 UART_PutChar('c');
                 UART_PutChar('i');
                 UART_PutChar('a');
-}
+                }
             }
-            
+           
             j = 0;
             
         } 
@@ -171,11 +161,10 @@ int main()
         MY_TIMER_REG_Write(1);
         
         MY_TIMER_REG_Write(0);
-        
-        
+
     }
     
-return 0;
+    return 0;
     
 }
 /* [] END OF FILE */
